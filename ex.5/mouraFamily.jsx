67a25e10b67d2a0664136a4d5504e4b1ba3e87c7
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-16 12:17:53 -0300

    feat: child components

---
+++ ex.5/mouraFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='João' lastName='Moura' />
+        <Member name='Maria' lastName='Moura' />
+        <Member name='Jose' lastName='Moura' />
+        <Member name='Pedro' lastName='Moura' />
+    </div>
+)
